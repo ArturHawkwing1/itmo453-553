8cebca937150ae2caa2f2ec7236653de590217ce
Dean Nelson <Dean@SecuredByFire.com>
2017-04-17 15:17:50 -0500

    moved sed lines into git repo edits since now cloneing from my forked repo and not Jeremy's base.

--- riemann/riemannmc/examplecom/etc/email.clj
+++ riemann/riemannmc/examplecom/etc/email.clj
@@ -1,6 +1,6 @@
 (ns examplecom.etc.email
   (:require [riemann.email :refer :all]))
 
-(def email (mailer {:host "smtp.gmail.com" :user "you@hawk.iit.edu" :pass "lovebunnies" :tls true :port 587 :from "riemann@example.com"}))
+(def email (mailer {:host "smtp.gmail.com" :user "SolarusExcantum@gmail.com" :pass "lymAKb5Tj1Zab4AUwkVsgZf0fYsyNj" :tls true :port 587 :from "dnelson8@hawk.iit.edu"}))
 ; URL to IIT student email https://ots.iit.edu/email/student-mail
 
